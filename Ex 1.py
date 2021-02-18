831df4e0225867a142f0482f4192ade2a5669405
Abssssss1753 <77192440+Abssssss1753@users.noreply.github.com>
2021-02-18 11:29:32 +0530

    Add files via upload

---
+++ Ex 1.py
@@ -0,0 +1,34 @@
+
+# create a dictionary and take input form the user and return the meaning of the word from the dictionary
+"""
+dis = {"Abandon":" The wrod is full of abandoned mines\n",
+       "Set":"Capable of change or of being changed\n",
+       "Chilax":"Calm dowm and relax\n",
+       "Buzzkill":"Person or things that has a depressing effect\n",
+       "Agender":"People do not identify as male or female\n",}
+
+print("enter the word you want the meaning")
+key = input()
+print(dis[key])
+
+print("enter the word you want the meaning")
+key1 = input()
+print(dis[key1])
+
+print("enter the word you want the meaning")
+key2 = input()
+print(dis[key2])
+
+print("enter the word you want the meaning")
+key3 = input()
+print(dis[key3])
+
+print("enter the word you want the meaning")
+key4 = input()
+print(dis[key4])
+
+print("enter the word you want the meaning")
+key5 = input()
+print(dis[key5])
+
+"""
